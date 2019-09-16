a2f32c88450883f266777104259b53419016c527
StMSoAP <55038102+StMSoAP@users.noreply.github.com>
2019-09-16 09:32:07 -0700

    Add files via upload

--- problem12.py
+++ problem12.py
@@ -1,19 +1,25 @@
 #generate triangle number
 #find factors
 #check number of factors
+import time
+start = time.time()
+triangle = int (0)
 
-for guess in range(1, 10000):
+for guess in range(1, 100000):
     factor_list = []
     
-    triangle = sum(range(1, guess+1)) 
+    triangle = triangle + guess 
     
-    for factor in range(1, triangle+1):
-        if triangle % factor == 0:
-            factor_list.append(factor)
+    #for factor in range(1, triangle+1):
+        #if triangle % factor == 0:
+            #factor_list.append(factor)
+    factor_list.extend([yield factor for factor in range(1, triangle+1) if factor * triangle // factor == triangle])
     if len(factor_list) > 499:
         break
         
     print(len(factor_list), end="\n")
+stop = time.time()
 print(factor_list)
 print(triangle)
+print("Time: {} seconds".format(stop - start))
         
